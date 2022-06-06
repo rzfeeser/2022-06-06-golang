21911883b6453f6a020d7d48c0498499dff773f1
rzfeeser <rzfeeser@noreply.github.com>
2022-06-06 14:01:26 +0000

    go variables

---
+++ exported-names.go
@@ -0,0 +1,14 @@
+/* Alta3 Research | RZFeeser
+   Exported names   */
+   
+package main
+
+import (
+    "fmt"
+    "math"
+)
+
+func main() {
+    fmt.Println(math.pi)   // notice that "Pi" is capitalized
+}
+
